2ae4d9c30590341b74f5d475a7ae5f78677df808
NathanRizza <56983564+NathanRizza@users.noreply.github.com>
2022-10-28 16:50:26 -0400

    Added rgb to hex functionality

--- img_to_theme.py
+++ img_to_theme.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import numpy as np
 
+def rgb_to_hex(rgb):
+    rgb2hex = lambda r,g,b: '#%02x%02x%02x' %(r,g,b)
+    return [ rgb2hex(*rgb[i,:]) for i in range(rgb.shape[0]) ]
 
 filename = sys.argv[1]
 
@@ -25,7 +28,7 @@ image_array = image_array.reshape(image_pixels, 3)
 
 n_clust = 8; # for plamora currently, will change in future to make it general
 
-print('Generating Theme For: ', filename , " please wait..." )
+print('Generating Theme For: ', filename , " please wait...")
 
 kmeans_output = KMeans(n_clusters=n_clust, random_state=0 ,n_init = 1, max_iter = 10).fit(image_array)
 
@@ -35,12 +38,11 @@ Average_Color_Int = (kmeans_output.cluster_centers_).astype(int)
 
 print("Theme:")
 print(Average_Color_Int)
-
+print(rgb_to_hex(Average_Color_Int))
 color_img = np.tile(0, (n_clust*100,100 ,3))
 
 color_img[150][50][1] = 0;
 for i in range (0,n_clust):
-    print(range (i*100,(i*100)+100))
     for j in range (0,3):
         for k in range (i*100,(i*100)+100):
             for l in range (0,100):
